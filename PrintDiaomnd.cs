83fc81032c23aab37398350200445850af6f3733
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-30 19:18:14 +0530

    Program to print diamond of numbers

---
+++ PrintDiaomnd.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PrintDiomandDemo
+{
+    public class Pattern
+    {
+        public void PrintPatern(int n)
+        {
+            //int m = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(k);
+
+                }
+                for (int m = i - 1; m >= 1; m--)
+                {
+                    Console.Write(m);
+                }
+
+
+                Console.WriteLine();
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+
+                for (int k = n - i; k >= 1; k--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                for (int m = i - 1; m >= 1; m--)
+                {
+                    Console.Write(m);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class PrintDiaomnd
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter Number :");
+            n = Convert.ToInt32(Console.ReadLine());
+            Pattern p = new Pattern();
+            p.PrintPatern(n);
+        }
+    }
+}
